84b2290fe985f1bcbf41e02f98cc6c083aec17d4
Raoul Koopman <raoul@fubo.tv>
2021-12-13 17:09:34 -0500

    Initial Commit

---
+++ The Lasso Effect/The_Lasso_EffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Lasso_EffectApp.swift
+//  The Lasso Effect
+//
+//  Created by Raoul Koopman on 12/13/21.
+//
+
+import SwiftUI
+
+@main
+struct The_Lasso_EffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
